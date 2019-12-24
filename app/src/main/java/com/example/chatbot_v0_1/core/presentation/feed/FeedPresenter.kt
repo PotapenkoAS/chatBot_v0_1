f52ea982061a3a9c34931e3ad5c43b9ca89bf9e7
PotapenkoAS <PotapenkoAC@gmail.com>
2019-12-24 21:41:53 +0300

    small

--- app/src/main/java/com/example/chatbot_v0_1/core/presentation/feed/FeedPresenter.kt
+++ app/src/main/java/com/example/chatbot_v0_1/core/presentation/feed/FeedPresenter.kt
@@ -1,13 +1,7 @@
 package com.example.chatbot_v0_1.core.presentation.feed
 
-import android.content.Intent
-import android.net.Uri
-import android.util.Log
-import android.view.View
-import androidx.core.content.ContextCompat.startActivity
 import com.example.chatbot_v0_1.core.domain.entity.FeedItem
 import com.example.chatbot_v0_1.core.domain.usecase.FeedUseCase
-import com.example.chatbot_v0_1.di.globalContext
 import com.hannesdorfmann.mosby3.mvp.MvpBasePresenter
 import org.koin.core.KoinComponent
 import org.koin.core.inject
@@ -33,7 +27,6 @@ class FeedPresenter : KoinComponent,
 
 
     private fun processResponse(view: FeedContract.View, response: ArrayList<FeedItem>?) {
-        Log.d("SHIT", "gdafjdksafdsafdsafjdsajkfhvdlajfidosalhfid")
         if (response != null)
             view.showNews(response)
     }
